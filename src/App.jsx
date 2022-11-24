f3ef6aec5c1e4bbd6c397c68783db8b9766e2e81
ChristianSpeziali <spezialichristian@gmail.com>
2022-11-24 15:53:26 -0300

    usuario inexistente

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
 import { Container } from "@mui/material";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Searcher from "./components/Searcher";
 import UserCard from "./containers/userCard";
 import { getGithubUser } from "./services/users";
 
-
 function App() {
-
-  const [inputUser, setInputUser] = useState('octocat')
-  const [userState, setUserState] = useState('inputuser')
-  const [notFound, setNotFound] = useState(false)
-
-  const gettingUser = async (user)=>{
-    const userResponse = await getGithubUser(user)
-    if (userState === "octocat"){
-      localStorage.setItem('octocat', userResponse)
-    }
-    if(userResponse.message === 'Not Found'){
-      const [ octocat ] = localStorage
-      setInputUser(octocat)
-      setNotFound(true)
-    } else {
-      setUserState(userResponse)
-    }
-  }
-  console.log(userState)
+  const [inputUser, setInputUser] = useState("octocat"); //captura del input
+  const [userState, setUserState] = useState("inputuser");
+  const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
-    gettingUser(inputUser)
-  }, [inputUser])
-  
+    gettingUser(inputUser);
+  }, [inputUser]);
+
+  const gettingUser = async (user) => {
+    const userResponse = await getGithubUser(user);
+    console.log(userResponse);
+    if (userState === "octocat") {
+      localStorage.setItem("octocat", userResponse);
+    }
+    if (userResponse.message === "Not Found") {
+      // const [octocat] = localStorage;
+      // setInputUser(octocat);
+      setNotFound(true);
+    } else {
+      setUserState(userResponse);
+      setNotFound(false);
+    }
+  };
+  // console.log(userState)
 
   return (
     <Container
@@ -44,8 +43,12 @@ function App() {
         alignItems: "center",
       }}
     >
-      <Searcher inputUser={inputUser} setInputUser={setInputUser}/>
-      <UserCard userState={userState} />
+      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+      {notFound === false ? (
+        <UserCard userState={userState} />
+        ) : (
+        <h1>Usuario no válido</h1>
+      )}
     </Container>
   );
 }
